4ee4629bfc9da6d7c56dd9b0a384b977eb5753fe
GwendalRaoul <47559821+GwendalRaoul@users.noreply.github.com>
2019-06-18 15:16:52 +0300

    Protobuf: Add a warning in case full python protobuf lib in use (#16)

--- wrappers/python/wirepas_messaging/__init__.py
+++ wrappers/python/wirepas_messaging/__init__.py
@@ -13,5 +13,21 @@ try:
 except ImportError:
     print("Could not import WPE handles")
 
+
+from google.protobuf.internal import api_implementation
+
+warning_msg = """
+***********************************************************************
+* WARNING:
+*     You are using the pure python protobuf implementation.
+*     For better results, please consider using the cpp version.
+*     For more information please consult:
+*     https://github.com/protocolbuffers/protobuf/tree/master/python
+***********************************************************************
+"""
+if api_implementation._default_implementation_type == "python":
+    print(warning_msg)
+
 __title__ = "wirepas_messaging"
 __version__ = "1.2.0rc1"
+
