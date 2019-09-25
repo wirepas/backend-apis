f7a6bc635cb50c5abdecbd8c813b6ec17f247a9d
teemupiiroinenwirepas <teemu.piiroinen@wirepas.com>
2019-09-25 15:25:50 +0300

    Add scratchpad status query message to WNT messages (#42)

--- wrappers/python/wirepas_messaging/wnt/ws_api/messagesbase.py
+++ wrappers/python/wirepas_messaging/wnt/ws_api/messagesbase.py
@@ -61,6 +61,7 @@ class MessagesBase(object):
         SET_NETWORK_DATA = 1071
         SEND_REMOTE_API_REQUEST = 1072
         SEND_DATA_MESSAGE = 1073
+        QUERY_SCRATCHPAD_STATUS = 1074
 
     class ProtocolVersions(enum.Enum):
         VERSION_2 = 2
