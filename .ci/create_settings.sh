9edb53b10514d91dee5181de60f6cc4384158180
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2019-10-05 10:47:29 +0300

    WNT Test server credentials (#70)
    This commit removes the plain text credentials from the
    repository as anyone (from a Travis machine) would
    be able to access the machine.

---
+++ .ci/create_settings.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [[ -f .secrets ]]
+then
+   set -o allexport
+   source .secrets
+   set +o allexport
+fi
+
+TEMPLATE=${1:-".ci/settings-template.yml"}
+OUTPUT=${2:-".settings.yml"}
+
+# create_settings
+#
+# copies and fills in the template
+# shellcheck disable=SC1090
+function create_settings
+{
+    local _TEMPLATE
+    local _OUTPUT
+
+    # input name is basename
+    _TEMPLATE=${1:-".ci/settings-template.yml"}
+    _OUTPUT=${2:-".settings.yml"}
+
+    rm -f "${_OUTPUT}" "${_OUTPUT}.tmp"
+    ( echo "cat <<EOF >${_OUTPUT}" && \
+      cat "${_TEMPLATE}" && \
+      echo "EOF" \
+    ) > "${_OUTPUT}.tmp"
+    . "${_OUTPUT}.tmp"
+    rm "${_OUTPUT}.tmp"
+}
+
+create_settings "${TEMPLATE}" "${OUTPUT}"
