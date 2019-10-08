fadf1f52c3ee00f555633b6c4d4d7d2e1964ad55
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2019-10-08 17:16:34 +0300

    Wheel contents and version bump (#79)
    Preparation for v1.3.0-rc.1.
    
    Several additional files have been added to the wheel file, such as:
    -  wnt examples and instructions on how to run them
    -  proto files used for the code generation
    
    This change close #45.

--- wrappers/python/wirepas_messaging/__about__.py
+++ wrappers/python/wirepas_messaging/__about__.py
@@ -20,7 +20,7 @@ __license__ = "Apache-2"
 __pkg_name__ = "wirepas_messaging"
 __title__ = "Wirepas Messaging Wrappers"
 __url__ = "https://github.com/wirepas/backend-apis/tree/master/wrappers/python"
-__version__ = "1.2.0"
+__version__ = "1.3.0-rc.1"
 __warning_msg__ = """
 ***********************************************************************
 * WARNING:
